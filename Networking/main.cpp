ddf93d4b7cf3d94d8ac56ba0ee9106cf9d974c4e
Andre <foreandr@gmail.com>
2021-12-13 00:58:28 -0500

    WORKING

--- Networking/main.cpp
+++ Networking/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "asio.h"
-using namespace std;
-using namespace boost::asio;
+#include "cfile.h"
+#include "unstd.h"
+#include <stdio.h>
+#include <sys/types.h>
+//#include <socketapi.h>
+#include <Ws2tcpip.h> // NO IDEA
+#include <winsock2.h>
 
+//using namespace std;
+//using namespace boost::asio;
 
+// TCP PARTICULAR
+#include <winsock2.h> // socket related | INET things
 int main() {
 	
+	// cREATE ONE SIDE OF DATA COMMUNCIATION
+	int network_socket;
+	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 
 }
