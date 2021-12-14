37fbb2cd607980723b068a43cf90a7d6869d10c5
Andre <foreandr@gmail.com>
2021-12-14 10:27:40 -0500

    number 3 seems to work properly

--- PythonNet4/client.py
+++ PythonNet4/client.py
@@ -8,4 +8,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # sits at an ip at a port
 
 s.connect((SERVER_IP, PORT))
 
-msg = s.recv(1024) # how much data can I recieve
+
+full_msg = ""
+while True:
+    msg = s.recv(8)  # how much data can I receive
+    if (len(msg)) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
+    #print(msg.decode("utf-8"))
+
+print(full_msg)
--- PythonNet4/server.py
+++ PythonNet4/server.py
@@ -5,11 +5,12 @@ PORT = 9002
 QUEUE_SIZE = 5  # for high traffic
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # sits at an ip at a port
-s.bind((socket.gethostname(), 9002))
+s.bind((HOST, 9002))
 s.listen(QUEUE_SIZE)
-print("Waiting for connection")
+print("Waiting for connection...")
 
 while True:
     client_socket, address = s.accept()  # address = where are they coming from
     print(f"connection from {address} as been established")
     client_socket.send(bytes("Welcome to server", "utf-8"))  # send information to them
+    client_socket.close()
